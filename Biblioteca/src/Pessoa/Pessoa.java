bb4dfb7693217a48d4c090beec3c29cee7fa07fd
Rosendo - PC <lucas-rosendo09@hotmail.com>
2019-01-06 12:29:56 -0300

    Adicionado pacote Pessoa e Email

--- Biblioteca/src/Pessoa/Pessoa.java
+++ Biblioteca/src/Pessoa/Pessoa.java
@@ -1,14 +1,206 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Pessoa;
 
 /**
- *
- * @author
+ * Classe Pessoa tem funcionalidade de gerenciar as pessoas
+ * cadastradas na biblioteca.
+ * @author lucas
+ * @version 1.0
  */
 public class Pessoa {
-    
+    private int codInscricao;
+    private String nome;
+    private String estado;
+    private String cidade;
+    private String bairro;
+    private String cep;
+    private String categoria;
+    private String email;
+    private String fone;
+    private String celular;
+    private String serie;
+    private String turno;
+    private String turma;
+
+    /**
+     * @return o codInscricao
+     */
+    public int getCodInscricao() {
+        return codInscricao;
+    }
+
+    /**
+     * @param codInscricao o codInscricao para definir
+     */
+    public void setCodInscricao(int codInscricao) {
+        this.codInscricao = codInscricao;
+    }
+
+    /**
+     * @return o nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome o nome para definir
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return o estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado o estado para definir
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return a cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade a cidade para definir
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    /**
+     * @return o bairro
+     */
+    public String getBairro() {
+        return bairro;
+    }
+
+    /**
+     * @param bairro o bairro para definir
+     */
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    /**
+     * @return o cep
+     */
+    public String getCep() {
+        return cep;
+    }
+
+    /**
+     * @param cep o cep para definir
+     */
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    /**
+     * @return a categoria
+     */
+    public String getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria a categoria para definir
+     */
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    /**
+     * @return o email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email o email para definir
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return o fone
+     */
+    public String getFone() {
+        return fone;
+    }
+
+    /**
+     * @param fone o fone para definir
+     */
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    /**
+     * @return o celular
+     */
+    public String getCelular() {
+        return celular;
+    }
+
+    /**
+     * @param celular o celular para definir
+     */
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    /**
+     * @return a serie
+     */
+    public String getSerie() {
+        return serie;
+    }
+
+    /**
+     * @param serie a serie para definir
+     */
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    /**
+     * @return o turno
+     */
+    public String getTurno() {
+        return turno;
+    }
+
+    /**
+     * @param turno o turno para definir
+     */
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    /**
+     * @return a turma
+     */
+    public String getTurma() {
+        return turma;
+    }
+
+    /**
+     * @param turma a turma para definir
+     */
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
 }
