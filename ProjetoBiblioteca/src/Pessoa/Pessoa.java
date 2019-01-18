8ddde67518ad0127792db5efd7343949607b1c9d
Rosendo - PC <lucas-rosendo09@hotmail.com>
2019-01-18 15:29:30 -0300

    Finalizado Pessoa parcialmente e adicionado Usuario

--- ProjetoBiblioteca/src/Pessoa/Pessoa.java
+++ ProjetoBiblioteca/src/Pessoa/Pessoa.java
@@ -1,18 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Pessoa;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
 
 /**
- *
- * @author lucas
+ * Classe para objetos do tipo Pessoa, onde serão contidos, valores e métodos para o mesmo.
+ * @author Lucas
+ * @version 1.5
  */
 public class Pessoa {
     private int codinsc;
@@ -31,234 +30,322 @@ public class Pessoa {
     private String nome;
     private int telefone;
     private String cep;
-
+    
     /**
-     * @return the codinsc
+     * Método contrutor da classe Pessoa.
+     * Responsavel por inicializar o objeto.
      */
+    public Pessoa() {
+    }
+    
+    /**
+     * Método contrutor completo da classe Pessoa.
+     * Responsavel por inicializar o objeto e todos seus metodos.
+     * @param codinsc - Código de inscrição da pessoa.
+     * @param usercode - Código do usuário.
+     * @param estado - Estado da residência da pessoa.
+     * @param numero - Nº da residência da pessoa.
+     * @param serie - Serie da Pessoa;
+     * @param endereco  - Endereço da residência da pessoa.
+     * @param turno - Turno da pessoa.
+     * @param bairro - Bairro da residência da pessoa.
+     * @param turma - Turma da pessoa.
+     * @param cidade - Cidade da residência da pessoa.
+     * @param categoria - Categoria da pessoa.
+     * @param email - Email da pessoa.
+     * @param celular - Celular da pessoa.
+     * @param nome - Nome da pessoa.
+     * @param telefone - Telefone da pessoa.
+     * @param cep - CEP da residência da pessoa.
+     */
+    public Pessoa(int codinsc, int usercode, String estado, int numero,
+            String serie, String endereco, String turno, String bairro,
+            String turma, String cidade, String categoria, String email,
+            int celular, String nome, int telefone, String cep) {
+        this.codinsc = codinsc;
+        this.usercode = usercode;
+        this.estado = estado;
+        this.numero = numero;
+        this.serie = serie;
+        this.endereco = endereco;
+        this.turno = turno;
+        this.bairro = bairro;
+        this.turma = turma;
+        this.cidade = cidade;
+        this.categoria = categoria;
+        this.email = email;
+        this.celular = celular;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.cep = cep;
+    }
+
+    
+    
+    /**
+    * Método para acessar o codinsc.
+    * @return int - Codigo da pessoa.
+    */
     public int getCodinsc() {
         return codinsc;
     }
 
     /**
-     * @param codinsc the codinsc to set
-     */
+    * Método para modificar o codinsc.
+    * @param codinsc - Codigo da pessoa
+    */
     public void setCodinsc(int codinsc) {
         this.codinsc = codinsc;
     }
 
     /**
-     * @return the usercode
-     */
+    * Método para acessar o usercode.
+    * @return int - Codigo do usuario.
+    */
     public int getUsercode() {
         return usercode;
     }
 
     /**
-     * @param usercode the usercode to set
-     */
+    * Método para modificar o usercode.
+    * @param usercode - Codigo do usuario.
+    */
     public void setUsercode(int usercode) {
         this.usercode = usercode;
     }
 
     /**
-     * @return the estado
-     */
+    * Método para acessar o estado.
+    * @return String - Estado do usuario.
+    */
     public String getEstado() {
         return estado;
     }
 
     /**
-     * @param estado the estado to set
-     */
+    * Método para modificar o estado.
+    * @param estado - Estado do usuario.
+    */
     public void setEstado(String estado) {
         this.estado = estado;
     }
 
     /**
-     * @return the numero
-     */
+    * Método para acessar o numero.
+    * @return int - Nº da residência.
+    */
     public int getNumero() {
         return numero;
     }
 
     /**
-     * @param numero the numero to set
-     */
+    * Método para modificar o numero.
+    * @param numero - Nº da residência.
+    */
     public void setNumero(int numero) {
         this.numero = numero;
     }
 
     /**
-     * @return the serie
-     */
+    * Método para acessar a serie.
+    * @return String - serie da pessoa(aluno).
+    */
     public String getSerie() {
         return serie;
     }
 
     /**
-     * @param serie the serie to set
-     */
+    * Método para modificar o numero.
+    * @param serie - série da pessoa(aluno).
+    */
     public void setSerie(String serie) {
         this.serie = serie;
     }
 
     /**
-     * @return the endereco
-     */
+    * Método para acessar a endereco.
+    * @return String - Endereço da pessoa.
+    */
     public String getEndereco() {
         return endereco;
     }
 
     /**
-     * @param endereco the endereco to set
-     */
+    * Método para modificar o endereco.
+    * @param endereco - Endereço da pessoa.
+    */
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
 
     /**
-     * @return the turno
-     */
+    * Método para acessar o turno.
+    * @return String - Turno da pessoa.
+    */
     public String getTurno() {
         return turno;
     }
 
     /**
-     * @param turno the turno to set
-     */
+    * Método para modificar o turno.
+    * @param turno - Turno da pessoa.
+    */
     public void setTurno(String turno) {
         this.turno = turno;
     }
 
     /**
-     * @return the bairro
-     */
+    * Método para acessar o bairro.
+    * @return String - Bairro da pessoa.
+    */
     public String getBairro() {
         return bairro;
     }
 
     /**
-     * @param bairro the bairro to set
-     */
+    * Método para modificar o bairro.
+    * @param bairro - Bairro da pessoa.
+    */
     public void setBairro(String bairro) {
         this.bairro = bairro;
     }
 
     /**
-     * @return the turma
-     */
+    * Método para acessar a turma.
+    * @return String - Turma da pessoa.
+    */
     public String getTurma() {
         return turma;
     }
 
     /**
-     * @param turma the turma to set
-     */
+    * Método para modificar a turma.
+    * @param turma - Turma da pessoa.
+    */
     public void setTurma(String turma) {
         this.turma = turma;
     }
 
     /**
-     * @return the cidade
-     */
+    * Método para acessar a cidade.
+    * @return String - Cidade da pessoa.
+    */
     public String getCidade() {
         return cidade;
     }
 
     /**
-     * @param cidade the cidade to set
-     */
+    * Método para modificar a cidade.
+    * @param cidade - Cidade da pessoa.
+    */
     public void setCidade(String cidade) {
         this.cidade = cidade;
     }
 
     /**
-     * @return the categoria
-     */
+    * Método para acessar a categoria.
+    * @return String - Categoria da pessoa.
+    */
     public String getCategoria() {
         return categoria;
     }
 
     /**
-     * @param categoria the categoria to set
-     */
+    * Método para modificar a categoria.
+    * @param categoria - Categoria da pessoa.
+    */
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
 
     /**
-     * @return the email
-     */
+    * Método para acessar a Email.
+    * @return String - Email da pessoa.
+    */
     public String getEmail() {
         return email;
     }
 
     /**
-     * @param email the email to set
-     */
+    * Método para modificar a email.
+    * @param email - Email da pessoa.
+    */
     public void setEmail(String email) {
         this.email = email;
     }
 
     /**
-     * @return the celular
-     */
+    * Método para acessar a Celular.
+    * @return int - Celular da pessoa.
+    */
     public int getCelular() {
         return celular;
     }
 
     /**
-     * @param celular the celular to set
-     */
+    * Método para modificar a celular.
+    * @param celular - Celular da pessoa.
+    */
     public void setCelular(int celular) {
         this.celular = celular;
     }
 
     /**
-     * @return the nome
-     */
+    * Método para acessar a Nome.
+    * @return String - Nome da pessoa.
+    */
     public String getNome() {
         return nome;
     }
 
     /**
-     * @param nome the nome to set
-     */
+    * Método para modificar a nome.
+    * @param nome - Nome da pessoa.
+    */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
     /**
-     * @return the telefone
-     */
+    * Método para acessar a Telefone.
+    * @return int - Telefone da pessoa.
+    */
     public int getTelefone() {
         return telefone;
     }
 
     /**
-     * @param telefone the telefone to set
-     */
+    * Método para modificar a telefone.
+    * @param telefone - Telefone da pessoa.
+    */
     public void setTelefone(int telefone) {
         this.telefone = telefone;
     }
 
     /**
-     * @return the cep
-     */
+    * Método para acessar a CEP.
+    * @return int - CEP da pessoa.
+    */
     public String getCep() {
         return cep;
     }
 
     /**
-     * @param cep the cep to set
-     */
+    * Método para modificar a cep.
+    * @param cep - CEP da pessoa.
+    */
     public void setCep(String cep) {
         this.cep = cep;
     }
     
+    /**
+     * Metodo responsável por salvar o objeto pessoa no banco de dados.
+     * A operação é realizada utilizando hibernate.
+     * @return boolean - Caso a operação for realizada com sucesso retorna true, caso contrário retorna false.
+     */
     public boolean salvarPessoa(){
         try{
-            SessionFactory fabrica = new Configuration().configure("Pessoa/hibernate.cfg.xml").buildSessionFactory();
+            SessionFactory fabrica = new Configuration().configure("Hibernate/hibernate.cfg.xml").buildSessionFactory();
             Session sessao = fabrica.openSession();
             Transaction tx_part = sessao.beginTransaction();
             sessao.save(this);
@@ -268,6 +355,103 @@ public class Pessoa {
         }
         catch(Exception e){
             return false;
+        }
+    }
+    /**
+     * Metodo responsável por atualizar o objeto pessoa no banco de dados.
+     * A operação é realizada utilizando hibernate.
+     * @return boolean - Caso a operação for realizada com sucesso retorna true, caso contrário retorna false.
+     */
+    public boolean atualizaPessoa(){
+        try{
+            SessionFactory fabrica = new Configuration().configure("Hibernate/hibernate.cfg.xml").buildSessionFactory();
+            Session sessao = fabrica.openSession();
+            Transaction tx_part = sessao.beginTransaction();
+            sessao.update(this);
+            tx_part.commit();
+            sessao.close();
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+    /**
+     * Metodo responsável por listar os objetos pessoa salvos no banco de dados.
+     * A operação é realizada utilizando hibernate.
+     * @return List - Caso a operação for realizada com sucesso retorna uma lista de pessoa, caso contrário retorna null.
+     */
+    public List ListaDePessoa(){
+        List<Pessoa> lista_Pessoa;
+        try{
+            SessionFactory fabrica = new Configuration().configure("Hibernate/hibernate.cfg.xml").buildSessionFactory();
+            Session sessao = fabrica.openSession();
+            lista_Pessoa = new ArrayList();
+            lista_Pessoa = sessao.createQuery("from Pessoa.Pessoa").list();
+            sessao.close();
+            return lista_Pessoa;
+        }
+        catch(Exception e){
+            return null;
+        }
+    }
+    /**
+     * Metodo responsável por excluir o objeto pessoa do banco de dados.
+     * A operação é realizada utilizando hibernate.
+     * @return boolean - Caso a operação for realizada com sucesso retorna true, caso contrário retorna false.
+     */
+    public boolean excluirPessoa(){
+        try{
+            SessionFactory fabrica = new Configuration().configure("Hibernate/hibernate.cfg.xml").buildSessionFactory();
+            Session sessao = fabrica.openSession();
+            Transaction tx_part = sessao.beginTransaction();
+            sessao.delete(this);
+            tx_part.commit();
+            sessao.close();
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+    
+    /**
+     * Metodo responsável por buscar o objeto pessoa salvo no banco de dados pelo codinsc.
+     * A operação é realizada utilizando hibernate.
+     * @param codInsc - Codigo de inscrição da pessoa.
+     * @return List - Caso a operação for realizada com sucesso retorna uma lista de pessoa, caso contrário retorna null.
+     */
+    public Pessoa buscarPessoa(int codInsc){
+        try{
+            SessionFactory fabrica = new Configuration().configure("Hibernate/hibernate.cfg.xml").buildSessionFactory();
+            Session sessao = fabrica.openSession();
+            Pessoa p =(Pessoa) sessao.createCriteria(Pessoa.class).add(Restrictions.eq("codinsc", codInsc)).uniqueResult();
+            sessao.close();
+            return p;
+        }
+        catch(Exception e){
+            return null;
+        }
+    }
+    
+    /**
+     * Metodo responsável por filtrar uma lista dos objetos pessoa salvos no banco de dados.
+     * A operação é realizada utilizando hibernate.
+     * @param filtro - Filtro da lista.
+     * @param coluna - Coluna da tabela.
+     * @return List - Caso a operação for realizada com sucesso retorna uma lista de pessoa, caso contrário retorna null.
+     */
+    public List filtrarPessoa(String coluna, String filtro){
+        try{
+            SessionFactory fabrica = new Configuration().configure("Hibernate/hibernate.cfg.xml").buildSessionFactory();
+            Session sessao = fabrica.openSession();
+            List<Pessoa> listaPessoa = new ArrayList();
+            listaPessoa = sessao.createCriteria(Pessoa.class).add(Restrictions.eq(coluna, filtro)).list();
+            sessao.close();
+            return listaPessoa;
+        }
+        catch(Exception e){
+            return null;
         }
     }
 }
--- ProjetoBiblioteca/src/Pessoa/testePessoa.java
+++ ProjetoBiblioteca/src/Pessoa/testePessoa.java
@@ -5,6 +5,7 @@
  */
 package Pessoa;
 
+import java.util.List;
 /**
  *
  * @author lucas
@@ -18,19 +19,23 @@ public class testePessoa {
             p.setCelular(99695866);
             p.setCep("099");
             p.setCidade("pdf");
-            p.setCodinsc(3335);
+            p.setCodinsc(3334);
             p.setEmail("lucas-rosendo@hotmail.com");
             p.setEndereco("RN-PDF");
             p.setEstado("RN");
             p.setNome("rosendo");
-            p.setNumero(171);
+            p.setNumero(9);
             p.setSerie("b");
             p.setTelefone(40028922);
             p.setTurma("1");
             p.setTurno("nocturne");
             p.setUsercode(1111);
-            if(p.salvarPessoa()) System.out.println("OK");
-            else System.err.println("ERRO");
+            p.salvarPessoa();
+            Pessoa temp = new Pessoa();
+            List<Pessoa> lista = temp.filtrarPessoa("nome", "rosendo");
+            temp = lista.get(0);
+            System.out.println(temp.getCodinsc()+ "\n" +temp.getNome());
+            
     }
     
 }
